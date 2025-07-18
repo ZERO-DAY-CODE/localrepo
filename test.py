6688acec2d82404b10d194167cf212b979023029
AKAS7991 <akash.prajapatiak450@gmail.com>
2025-07-18 08:10:17 +0530

    comit

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("Hello World")
+class Akash:
+    name = "akash"
+    age = 20
+    college = "DDU"
+    def cal(self,x,y):
+        self.x=x
+        self.y= y
+        c=x+y
+        return c
+
+detail = Akash()
+print(detail.name,detail.cal(10,20))
+
+
